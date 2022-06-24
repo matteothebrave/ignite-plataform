8b1145b2bc2463064010c44b64ba04d0b74fcb25
Matteo <thebravematteo33@protonmail.com>
2022-06-23 22:25:01 -0300

    comitting videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { Event } from "./pages/Event"
 
+import { Router } from "./Router"
+import { ApolloProvider } from "@apollo/client"
+import { client } from "./lib/apollo"
+import { BrowserRouter } from "react-router-dom"
 
 
 function App() {
   return (
-    <div>
-               <Event />
-    </div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+        </BrowserRouter>
+      </ApolloProvider>
   )
 }
 
